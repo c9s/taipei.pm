eb0f4a7d09bc66b2b15567357d5bee408f388d6a
c9s <cornelius.howl@gmail.com>
2010-04-27 03:37:47 +0800

    header and footer

--- lib/Taipei/View.pm
+++ lib/Taipei/View.pm
@@ -4,12 +4,19 @@ use strict;
 use Jifty::View::Declare -base;
 use Jifty::View::Declare::Helpers;
 
+template 'app_header' => sub {
+
+};
+
+template 'app_footer' => sub {
+
+};
+
 template 'index.html' => page {
 
 
 
 
 };
-
 
 1;
